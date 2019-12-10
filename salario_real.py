7c631c9508e75d3802941dc021157c4669b30bf8
Thiago Araujo <webhead17@gmail.com>
2019-12-09 21:27:31 -0300

    novas alterações

--- salario_real.py
+++ salario_real.py
@@ -1,8 +1,13 @@
 # salario_real.py
 salario = int(input('Digite seu salário: '))
-imposto = float(input('Digite seu imposto em % (ex: 27.5): '))
+imposto = input('Digite seu imposto em % (ex: 27.5): ')
 
 #imposto = float(imposto) if imposto else 27.5
+
+if not imposto:
+    imposto = 27.5
+else:
+    imposto = float(imposto)
 
 if  imposto < 10:
     print('Baixo')
@@ -10,11 +15,12 @@ if  imposto < 10:
 elif imposto >= 10. and imposto <=27:
     print('Médio')
 
-elif imposto > 27. and imposto < 100:
+elif imposto > 27. and imposto <= 100:
     print('Alto')
 
 else:
     print('Imposto inválido')
+    exit(1)
     #imposto = float(imposto)
 
 
