e152e5c37642fea47d881d2661bdd7cd2c4b97d0
Thiago Araujo <webhead17@gmail.com>
2019-12-09 21:36:20 -0300

    alterado para while

--- salario_real.py
+++ salario_real.py
@@ -1,28 +1,14 @@
 # salario_real.py
 salario = int(input('Digite seu salário: '))
-imposto = input('Digite seu imposto em % (ex: 27.5): ')
+imposto = 27.
 
-#imposto = float(imposto) if imposto else 27.5
+while imposto > 0.:
 
-if not imposto:
-    imposto = 27.5
-else:
-    imposto = float(imposto)
-
-if  imposto < 10:
-    print('Baixo')
+    imposto = input('Digite seu imposto em % (ex: 27.5): ')
+    if not imposto:
+        imposto = 27
+    else:
+        imposto = float(imposto)
     
-elif imposto >= 10. and imposto <=27:
-    print('Médio')
-
-elif imposto > 27. and imposto <= 100:
-    print('Alto')
-
-else:
-    print('Imposto inválido')
-    exit(1)
-    #imposto = float(imposto)
-
-
-
-print(f'Valor real: {salario - (salario * (imposto / 100))}')
+    print(f'Valor real: {salario - (salario * (imposto / 100))}')
+    
