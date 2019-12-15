d8a12d5b318b65744446bc9ef49eed356d6c5825
tcaraujo <52897075+tcaraujo@users.noreply.github.com>
2019-12-15 16:41:42 -0300

    Update desafio_mario.py
    Adicionado segunda parte - '#' do lado direito

--- desafio_mario.py
+++ desafio_mario.py
@@ -1,6 +1,5 @@
 linhas = int(input('Digite a quantidade de linhas da piramide: ')) + 1
 
 for i in range(1, linhas):
-   print(' ' * (linhas - i), '#'  * i, sep="")
-
-
+   print(' ' * (linhas - i), '#'  * i, ' ', '#' * i, sep = "")
+   
